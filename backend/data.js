2309cf3127f0f528b26e32155ee75613b6a98e78
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-06 09:34:41 -0500

    initial commit

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+export default {
+    products: [{
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 50,
+        brand: 'Adidas',
+        rating: 4.2,
+        numReviews: 10
+    },
+    {
+        _id: '3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 70,
+        brand: 'Dockers',
+        rating: 4.1,
+        numReviews: 10
+    }, {
+        _id: '4',
+        name: 'Silk Shirt',
+        category: 'Shirt',
+        image: '/images/d1.jpg',
+        price: 200,
+        brand: 'Polo',
+        rating: 4.5,
+        numReviews: 13
+    }
+]
+}
