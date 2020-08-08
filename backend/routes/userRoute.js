cf001ffdaf8c982de5400716c5f14095e242132c
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-08 06:54:36 -0500

    fixed error in utils.js, had default in the export which was disallowing the getToken function.

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,37 +1,59 @@
-import express from "express";
-import User from "../models/userModel";
+import express from 'express';
+import User from '../models/userModel';
+import { getToken } from '../util';
 
 const router = express.Router();
 
-router.post('/siginin', async (req, res) => {
+router.post('/signin', async (req, res) => {
 
-  const signInUser = await User.findOne({
+  const signinUser = await User.findOne({
     email: req.body.email,
     password: req.body.password
   });
-  if(signInUser) {
+  if (signinUser) {
     res.send({
-      _id: signInUser.id,
-      name: signInUser.name,
-      email: signInUser.email,
-      isAdmin: signInUser.isAdmin,
-      token: getToken(user)
+      _id: signinUser.id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser)
     })
+
   } else {
-    res.status(401).send({msg: 'Invalid Email or Password.'});
+    res.status(401).send({ msg: 'Invalid Email or Password.' });
   }
 
-});
+})
+
+// router.post('/register', async (req, res) => {
+//   const user = new User({
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password
+//   });
+//   const newUser = await user.save();
+//   if (newUser) {
+//     res.send({
+//       _id: newUser.id,
+//       name: newUser.name,
+//       email: newUser.email,
+//       isAdmin: newUser.isAdmin,
+//       token: getToken(newUser)
+//     })
+//   } else {
+//     res.status(401).send({ msg: 'Invalid User Data.' });
+//   }
+
+// })
 
 router.get("/createadmin", async (req, res) => {
   try {
     const user = new User({
-      name: "Samuel",
-      email: "samklepdev@gmial.com",
-      password: "123456",
-      isAdmin: true,
+      name: 'Samuel',
+      email: 'samklepdev@gmail.com',
+      password: '1234',
+      isAdmin: true
     });
-
     const newUser = await user.save();
     res.send(newUser);
   } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,22 @@ import data from './data';
 import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import userRoute from './routes/userRoute';
 
 dotenv.config();
 
-const mongodburl =config.MONGODB_URL;
-mongoose.connect(mongodburl, {
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 }).catch(error => console.log(error.reason));
 
+
 const app = express();
-
+app.use(bodyParser.json());
 app.use("/api/users", userRoute);
-
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
   const product = data.products.find(x => x._id === productId);
@@ -28,11 +29,7 @@ app.get("/api/products/:id", (req, res) => {
 });
 
 app.get("/api/products", (req, res) => {
-    res.send(data.products);
+  res.send(data.products);
 });
 
-
-app.listen(5000, () => {
-    console.log("Server started on port localhost:5000");
-});
-
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
